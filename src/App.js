5408e95b5acf5420f30a601ffbdc5a3f686115d6
unknown <nitesh@pixlerlab.com>
2022-09-05 09:19:13 +0530

    forgot

--- src/App.js
+++ src/App.js
@@ -1,31 +1,23 @@
-import './App.css';
-import Home from './Pages/Home';
-import {Routes, Route} from 'react-router-dom'
-import SignUp from './Pages/SignUp';
-import Dashboard from './Pages/Dashboard';
-import Product from './Pages/Product';
-import Navbar from './Components/Navbar';
-import Category from './Pages/Category';
-
+import "./App.css";
+import Home from "./Pages/Home";
+import { Routes, Route } from "react-router-dom";
+import SignUp from "./Pages/SignUp";
+import Dashboard from "./Pages/Dashboard";
+import Product from "./Pages/Product";
+import Navbar from "./Components/Navbar";
+import Category from "./Pages/Category";
 
 function App() {
   return (
     <>
-    <Navbar/>
-    <Routes>
-    <Route excat path='/' element={<Home/>} />
-    <Route excat path='/signup' element={<SignUp/>} />
-    <Route excat path='/dashboard' element={<Dashboard/>} />
-    <Route  path='/products' element={<Product/>} />
-    <Route  path='/category' element={<Category/>} />
-
-    
-
-    
-
-  
-    </Routes>
-    
+      <Navbar />
+      <Routes>
+        <Route excat path="/" element={<Home />} />
+        <Route excat path="/signup" element={<SignUp />} />
+        <Route excat path="/dashboard" element={<Dashboard />} />
+        <Route path="/products" element={<Product />} />
+        <Route path="/category" element={<Category />} />
+      </Routes>
     </>
   );
 }
